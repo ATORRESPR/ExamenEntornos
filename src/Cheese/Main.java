3fee1d955f847b98a215117436cb57c5f449dd7b
ATORRESPR <a.torresprellezo@gmail.com>
2022-06-01 12:02:51 +0200

    COMMIT master MAIN

--- src/Cheese/Main.java
+++ src/Cheese/Main.java
@@ -6,6 +6,8 @@
  */
 package Cheese;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author ATORRESPR
@@ -15,5 +17,19 @@ public class Main {
      public static void main(String[] args) {
     
          System.out.println("1º Gdam: Entornos de desarrollo");
+     }       
+public static void ListadoProductos(ListaProductos a){
+    
+        ArrayList<Producto> Listado = new ArrayList<>();
+        Listado = a.getProductos();
+        System.out.print("Productos: ");
+        System.out.println("\t\tNum.Productos:"+Listado.size());
+        System.out.format("%-24s%-15s%-15s\n", "Codigo","Tipo");
+        System.out.println("-----------------------------------------------------");
+        for (Producto c : Listado){
+            System.out.format("%-24s%-15s%-15s\n",c.getcode(),c.getTipo());
+        }
+        System.out.println("-----------------------------------------------------");
     }
-}
+    }
+
